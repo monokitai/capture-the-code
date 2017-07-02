00b5bb139c54463d588408618319f385ae062f17
monokitai <monokitai@desktop-b8jsg4p.speedport_w_724v_09011603_05_010>
2017-07-02 07:51:19 +0200

    init version

---
+++ dp/the-coin-change-problem.java
@@ -0,0 +1,26 @@
+
+//
+//https://www.hackerrank.com/challenges/coin-change
+//
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+int change(int n, int m, int[] C){
+
+  int possibilities = 0;
+
+  
+  return possibilities;
+}
+
+public static void main(String[] args){
+
+  BufferedReader br = new BufferedReader(new InputStreamReader(new System.in));
+  String in;
+  
+  
+  return 0;
+}
+
+
