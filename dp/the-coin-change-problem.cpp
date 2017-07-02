c6277a79f76f8e7d8d8bf11e760211602ebe9dc9
monokitai <monokitai@desktop-b8jsg4p.speedport_w_724v_09011603_05_010>
2017-07-02 07:43:43 +0200

    init version

---
+++ dp/the-coin-change-problem.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+int change(int n, int m, int *C){
+
+  int possibilities = 0;
+  
+  
+  return possibilities;
+}
+
+int main(int argc, char *argv[]){
+
+  
+  return 0;
+}
