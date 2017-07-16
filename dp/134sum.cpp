a7f5bda0d4cd7bfed4515d4897900aea9b0c7577
monokitai <monokitai@desktop-b8jsg4p.speedport_w_724v_09011603_05_010>
2017-07-16 20:49:48 +0200

    added algorithms

---
+++ dp/134sum.cpp
@@ -0,0 +1,22 @@
+
+//number of ways to write n as a sum of 1, 3 and 4
+
+#include <iostream>
+
+int ways(int n){
+
+  int *w = new int[n+1];
+  w[0] = w[1] = w[2] = 1;
+  w[3] = 2;
+  for(int i = 4; i <= n; i++){
+    w[i] = w[i-4] + w[i-3] + w[i-1];
+  }
+  return w[n];
+}
+
+int main(){
+  std::cout << ways(10) << std::endl;
+  return 0;
+}
+
+
